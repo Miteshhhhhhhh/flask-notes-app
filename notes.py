3ef1908c589b1c9b8b716986e6cf2a90dd134663
Miteshhhhhhhh <miteshhh45@gmail.com>
2026-06-10 16:00:30 +0530

    Add edit and delete functionality to Flask notes app

---
+++ notes.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request , redirect
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    conn = sqlite3.connect("notes.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM notes")
+    all_notes = cursor.fetchall()
+    conn.close()
+    return render_template("home.html", notes=all_notes)
+
+def init_db():
+    conn = sqlite3.connect("notes.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS notes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL)
+    """)
+    conn.commit()
+    conn.close()
+
+init_db()
+
+@app.route("/add", methods=["GET", "POST"])
+def add_note():
+    if request.method == "POST":
+        title = request.form['title']
+        content = request.form['content']
+
+        conn = sqlite3.connect("notes.db")
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO notes (title, content) VALUES (?,?)", (title, content))
+        conn.commit()
+        conn.close()
+        return redirect("/")
+    return render_template("add_note.html")
+
+
+@app.route("/delete/<int:note_id>")
+def delete_note(note_id):
+    conn = sqlite3.connect("notes.db")
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM notes WHERE id=?", (note_id,))
+    conn.commit()
+    conn.close()
+    return redirect("/")
+
+@app.route("/edit/<int:note_id>", methods=["GET", "POST"])
+def edit_note(note_id):
+    conn = sqlite3.connect("notes.db")
+    cursor = conn.cursor()
+
+    if request.method == "POST":
+        new_title = request.form['title']
+        new_content = request.form['content']
+
+        cursor.execute(
+            "UPDATE notes SET title=?, content=? WHERE id=?",
+            (new_title, new_content, note_id)
+        )
+        conn.commit()
+        conn.close()
+        return redirect("/")
+    cursor.execute("SELECT * FROM notes WHERE id=?", (note_id,))
+    note = cursor.fetchone()
+    conn.close()
+    return render_template("edit_note.html", note=note)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
